279a04324bc3815cf53d8691340c80d20a50032c
Joshua Wheeler <joshua.r.wheeler.27@gmail.com>
2021-09-20 14:07:52 -0500

    updated where_exercises.sql query

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,52 +1,61 @@
 -- 2. 709
-SELECT emp_no, first_name, last_name
+SELECT *
 FROM employees 
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 -- 3. It matches Q2, 709
-SELECT emp_no, first_name, last_name
+SELECT *
 FROM employees
 WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
--- 4. 612.
-SELECT emp_no, first_name, last_name
+-- 4. 441.
+SELECT *
 FROM employees
-WHERE gender = 'M' AND first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
 
 -- 5. 7330
-SELECT emp_no, first_name, last_name
+SELECT *
 FROM employees
 WHERE last_name LIKE ("E%");
 
 -- 6. 30723
-SELECT emp_no, first_name, last_name
+SELECT *
 FROM employees
 WHERE last_name LIKE ("E%") OR last_name LIKE ("%e");
 
 -- 7. 899
-SELECT emp_no, first_name, last_name
+SELECT *
 FROM employees
 WHERE last_name LIKE ("E%") AND last_name LIKE ("%e");
+-- 24292
+SELECT *
+FROM employees
+WHERE last_name LIKE ("%e");
+
 
 -- 8. 135214
-SELECT emp_no, first_name, last_name, hire_date
+SELECT *
 FROM employees
 WHERE hire_date LIKE "199%";
+
 
 -- 9. 842
 SELECT *
 FROM employees
 WHERE birth_date LIKE "%12-25";
 
+
 -- 10. 362
 SELECT *
 FROM employees
 WHERE hire_date LIKE "199%" AND birth_date LIKE "%12-25";
 
+
 -- 11. 1873
 SELECT *
 FROM employees
 WHERE last_name LIKE "%q%";
+
 
 -- 12. 547
 SELECT *
