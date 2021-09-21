8a1785fa1b1896527c7de26afe1dd760c6d186a6
Joshua Wheeler <joshua.r.wheeler.27@gmail.com>
2021-09-21 09:12:35 -0500

    Updated limit.sql query.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -13,9 +13,15 @@ WHERE hire_date LIKE "199%" AND birth_date LIKE "%12-25"
 ORDER BY hire_date ASC
 LIMIT 5;
 
--- 4. LIMIT is how many results per page, OFFSET changes the limited results displayed by skipping hte designated amount, page number is the OFFSET divided by the LIMIT. 
+-- 4. LIMIT is how many results per page, OFFSET changes the limited results displayed by skipping the designated amount, page number is the OFFSET divided by the LIMIT. 
 SELECT *
 FROM employees
 WHERE hire_date LIKE "199%" AND birth_date LIKE "%12-25"
 ORDER BY hire_date ASC
 LIMIT 5 OFFSET 50;
+
+/* 231580	1957-12-25	Christophe	Baca	F	1990-08-11
+275621	1961-12-25	Moie	Birsak	M	1990-08-11
+494492	1957-12-25	Chikako	Ibel	F	1990-08-12
+48396	1955-12-25	Shounak	Jansen	M	1990-08-13
+17351	1961-12-25	Zhigen	Boissier	M	1990-08-18 */
