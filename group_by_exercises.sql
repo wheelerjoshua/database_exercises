23315de106e13f20b4abba4de5b5ad6a3699c835
Joshua Wheeler <joshua.r.wheeler.27@gmail.com>
2021-09-22 10:13:51 -0500

    updated group_by_exercises.sql query

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -38,14 +38,14 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 GROUP BY first_name, gender
 ORDER BY first_name;
 
--- 8. 
+-- 8. Yes, there are duplicates
 SELECT LOWER(CONCAT(
 	SUBSTR(first_name, 1,1),
 	SUBSTR(last_name, 1,4), 
 	"_",
 	SUBSTR(birth_date, 9, 2),
 	SUBSTR(birth_date, 3, 2)
-)) AS username, COUNT(*) AS how_many
+)) AS username, COUNT(*) as how_many
 FROM employees
 GROUP BY username
 ORDER BY how_many DESC;
@@ -63,39 +63,43 @@ GROUP BY username
 HAVING amount > 1
 ORDER BY amount DESC;
 
--- 9a. 
-SELECT AVG(salary)
-FROM salaries
-WHERE from_date < NOW();
+-- 9. 
 
+-- Historic
+SELECT AVG(salary)
+FROM salaries;
+-- WHERE from_date < NOW()
+
+-- Current 
 SELECT AVG(salary)
 FROM salaries
 WHERE to_date > NOW();
 
--- 9b. 
+-- 10. 
 SELECT emp_no, AVG(salary)
 FROM salaries
-WHERE from_date < NOW()
+-- WHERE from_date < NOW()
 GROUP BY emp_no;
 
--- 9c.
+
+-- 11.
 SELECT emp_no, AVG(salary)
 FROM salaries
 WHERE to_date > NOW()
 GROUP BY emp_no;
 
--- 9d. 
+-- 12. 
 SELECT emp_no, MAX(salary)
 FROM salaries
 GROUP BY emp_no;
 
--- 9e.
+-- 13.
 SELECT emp_no, MAX(salary) AS max
 FROM salaries
 GROUP BY emp_no
 HAVING max > 150000;
 
--- 9f. 
+-- 14. 
 SELECT emp_no, AVG(salary) AS average
 FROM salaries
 GROUP BY emp_no
